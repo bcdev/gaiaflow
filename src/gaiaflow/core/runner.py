22414b2935feecbb5390f217933539ce40c23987
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-09-04 17:34:31 +0200

    refactor runner.py

--- src/gaiaflow/core/runner.py
+++ src/gaiaflow/core/runner.py
@@ -9,6 +9,28 @@ import os
 import pickle
 from typing import Any
 
+def run(
+    func_path: str | None = None,
+    args: list | None = None,
+    kwargs: dict[str, Any] | None = None,
+) -> dict[str, str]:
+    mode = os.environ.get("MODE", "dev")
+    print(f"## Runner running in {mode} mode ##")
+    func_path, args, kwargs = _resolve_inputs(func_path, args, kwargs, mode)
+
+    if not func_path:
+        raise ValueError("func_path must be provided")
+
+    func = _import_function(func_path)
+
+    print(f"Running {func_path} with args: {args} and kwargs :{kwargs}")
+    result = func(*args, **kwargs)
+    print("Function result:", result)
+
+    _write_result(result, mode)
+
+    return result
+
 
 def _extract_params_from_env(prefix="PARAMS_") -> dict[str, str]:
     return {
@@ -17,48 +39,29 @@ def _extract_params_from_env(prefix="PARAMS_") -> dict[str, str]:
         if k.startswith(prefix)
     }
 
-
-def run(
-    func_path: str | None = None,
-    args: list | None = None,
-    kwargs: dict[str, Any] | None = None,
-) -> dict[str, str]:
-    mode = os.environ.get("MODE", "dev")
-    print(f"## Runner running in {mode} mode ##")
+def _resolve_inputs(func_path: str, args: list[Any], kwargs: dict[Any],
+                   mode: str):
     if mode == "dev":
-        print("args", args)
-        print("kwargs", kwargs)
-    else:
-        func_path = os.environ.get("FUNC_PATH", "")
+        return func_path, args or [], kwargs or {}
+    else: # all other modes (dev_docker, prod_local and prod)
+        func_path = os.environ.get("FUNC_PATH", func_path)
         args = json.loads(os.environ.get("FUNC_ARGS", "[]"))
         kwargs = json.loads(os.environ.get("FUNC_KWARGS", "{}"))
-        params: dict = _extract_params_from_env()
-        kwargs["params"] = params
-        print("args", args)
-        print("kwargs", kwargs)
+        kwargs["params"] = _extract_params_from_env()
+        return func_path, args, kwargs
 
-    if not func_path:
-        raise ValueError("func_path must be provided")
-
-    module_path, func_name = func_path.rsplit(":", 1)
+def _import_function(func_path: str):
     import importlib
-
+    module_path, func_name = func_path.rsplit(":", 1)
     module = importlib.import_module(module_path)
-    func = getattr(module, func_name)
+    return getattr(module, func_name)
 
-    print(f"Running {func_path} with args: {args} and kwargs :{kwargs}")
-    result = func(*args, **kwargs)
-    print("Function result:", result)
+def _write_result(result, mode):
     if mode == "prod" or mode == "prod_local":
-        # This is needed when we use KubernetesPodOperator and want to
-        # share information via XCOM.
         _write_xcom_result(result)
     if mode == "dev_docker":
         with open("/tmp/script.out", "wb+") as tmp:
             pickle.dump(result, tmp)
-
-    return result
-
 
 def _write_xcom_result(result: Any) -> None:
     try:
@@ -67,12 +70,6 @@ def _write_xcom_result(result: Any) -> None:
 
         with open(f"{xcom_dir}/return.json", "w") as f:
             json.dump(result, f)
-
-        path = "/airflow/xcom/return.json"
-        print("[DEBUG] File exists:", os.path.exists(path))
-        print("[DEBUG] File size:", os.path.getsize(path))
-        with open(path, "r") as f:
-            print("[DEBUG] File contents:", f.read())
 
         print("Result written to XCom successfully")
     except Exception as e:
